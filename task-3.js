1a6690dbfa83999ccab9a34b9ed833a86c6c0485
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2021-08-10 21:30:57 +0300

    деструктуризація resolve

--- task-3.js
+++ task-3.js
@@ -2,15 +2,16 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
-  return new Promise((resorve, reject) => {
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
     const delay = randomIntegerFromInterval(200, 500);
-
+    let id = transaction.id;
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resorve(transaction.id, delay);
+        const result = {id, delay};
+        resolve(result);
       };
         reject(transaction.id);
       
@@ -18,8 +19,8 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ( result ) => {
+  console.log(`Transaction ${result.id} processed in ${result.delay}ms`);
 };
 
 const logError = id => {
